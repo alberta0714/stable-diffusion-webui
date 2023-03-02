daf598474d2133050df27cb46147ee111d8003cf
DESKTOP-OLVEMOF\Administrator <sunzhanchao@126.com>
2023-03-03 03:16:25 +0800

    ac

--- push_webui.sh
+++ push_webui.sh
@@ -3,8 +3,8 @@ git add .
 git commit -a -m "ac"
 git push
 #SSHREMOTE=root@152.136.186.81
-SSHREMOTE=ai@43.130.154.63
-ssh ${SSHREMOTE} 'cd /home/; git pull'
+#SSHREMOTE=ai@43.130.154.63
+#ssh ${SSHREMOTE} 'cd /home/; git pull'
 #ssh ${SSHREMOTE} 'cd /usr/local/code/onediff; python3 examples/text_to_image_sd21.py'
 ##ssh ${SSHREMOTE} 'cd /usr/local/code/onediff; python3 examples/text_to_image_chill.py'
 #
---
+++ sync.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+SSHREMOTE=root@43.130.154.63
+scp -r ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/out.tar.gz /e/ai/
+
+
+exit 0
