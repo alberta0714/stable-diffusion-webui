c874b614575ee0e350914483fc7c2dbf9090a214
sunzhanchao <sunzhanchao@igetcool.com>
2023-03-02 14:27:28 +0800

    ac

---
+++ push.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+git add .
+git commit -a -m "ac"
+git push
+#SSHREMOTE=root@152.136.186.81
+#
+#ssh ${SSHREMOTE} 'cd /usr/local/code/onediff; git pull'
+#ssh ${SSHREMOTE} 'cd /usr/local/code/onediff; python3 examples/text_to_image_sd21.py'
+##ssh ${SSHREMOTE} 'cd /usr/local/code/onediff; python3 examples/text_to_image_chill.py'
+#
+##scp ${SSHREMOTE}:/usr/local/code/onediff/cat_2_1_768.png /Users/sunzhanchao/stabilityai/
+#scp ${SSHREMOTE}:/usr/local/code/onediff/cat_2_1_768.png /d/usr/local/code/github/onedif_pics/
+#
+#ssh ${SSHREMOTE} 'cd /usr/local/code/onediff; rm -rf *.png'
+
+exit 0
