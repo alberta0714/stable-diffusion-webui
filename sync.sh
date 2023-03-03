006ca9971eaf7589cfea8147bd02abe2d7c9774f
DESKTOP-OLVEMOF\Administrator <sunzhanchao@126.com>
2023-03-03 08:21:14 +0000

    ac

--- sync.sh
+++ sync.sh
@@ -3,5 +3,5 @@ SSHREMOTE=root@43.130.154.63
 #scp -r ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/out.tar.gz /e/ai/
 #scp -r ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/extensions/sd-webui-additional-networks.tar.gz /e/ai/
 #rsync -rvth ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/outputs/ /Users/sunzhanchao/Downloads/aiputputs/
-rsync -rvth ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/outputs/ /e/ai/outputs/
+rsync -rvth root@43.130.154.63:/data/code/github/stable-diffusion-webui/outputs/ /e/ai/aioutputs/
 exit 0
