3cc407f1ede6782302dd3e956b934250e8db7058
sunzhanchao <sunzhanchao@igetcool.com>
2023-03-03 15:39:49 +0800

    ac

--- sync.sh
+++ sync.sh
@@ -6,6 +6,7 @@ do
 #scp -r ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/extensions/sd-webui-additional-networks.tar.gz /e/ai/
 rsync -rvth ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/outputs/ /Users/sunzhanchao/Downloads/aiputputs/
 #rsync -rvth ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/outputs/ /e/ai/outputs/
+echo "slee 15 seconds ..."
 sleep 15
 done
 exit 0
