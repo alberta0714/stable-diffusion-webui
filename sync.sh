eee7c3e60abd91ac432c7945496d86d8852f51b6
DESKTOP-OLVEMOF\Administrator <sunzhanchao@126.com>
2023-03-03 14:30:51 +0000

    ac

--- sync.sh
+++ sync.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 SSHREMOTE=root@43.130.154.63
-#while True
-#do
-  #scp -r ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/out.tar.gz /e/ai/
-  #scp -r ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/extensions/sd-webui-additional-networks.tar.gz /e/ai/
-  #rsync -rvth ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/outputs/ /Users/sunzhanchao/Downloads/aioutputs/
-  rsync -rvth ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/outputs/ /cygdrive/e/ai/aioutputs
-#echo "slee 15 seconds ..."
-#sleep 15
-#done
+while True
+do
+#scp -r ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/out.tar.gz /e/ai/
+#scp -r ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/extensions/sd-webui-additional-networks.tar.gz /e/ai/
+#rsync -rvth ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/outputs/ /Users/sunzhanchao/Downloads/aioutputs/
+rsync -rvth ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/outputs/ /cygdrive/e/ai/aioutputs
+echo "slee 15 seconds ..."
+sleep 15
+done
 exit 0
