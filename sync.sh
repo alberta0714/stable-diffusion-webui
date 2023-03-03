544c84a6f59b36da3a009e7bba6a54b3b027f435
sunzhanchao <sunzhanchao@igetcool.com>
2023-03-03 14:44:43 +0800

    ac

--- sync.sh
+++ sync.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 SSHREMOTE=root@43.130.154.63
 #scp -r ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/out.tar.gz /e/ai/
-rsync -rvth ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/outputs/ /Users/sunzhanchao/Downloads/aiputputs/
+#rsync -rvth ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/outputs/ /Users/sunzhanchao/Downloads/aiputputs/
+rsync -rvth ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/outputs/ /e/ai/outputs/
 exit 0
