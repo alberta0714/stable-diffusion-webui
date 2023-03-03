b9dfb9b58d65ec6a9e08fc78fe41814716fde5c5
sunzhanchao <sunzhanchao@igetcool.com>
2023-03-03 20:18:34 +0800

    no message

--- build.sh
+++ build.sh
@@ -49,7 +49,7 @@ wget -O koreanDollLikeness_v10.safetensors https://civitai.com/api/download/mode
 cd models/Stable-diffusion
 wget -O chilloutmix_NiPrunedFp32.safetensors https://civitai.com/api/download/models/9475 --no-check-certificate
 作者：jasoai https://www.bilibili.com/read/cv21955055 出处：bilibili
-
+wget -O chilloutmix_NiPrunedFp32Fix.safetensors https://civitai.com/api/download/models/9475 --no-check-certificate
 
 
 git clone https://github.com/kohya-ss/sd-webui-additional-networks extensions/sd-webui-additional-networks
@@ -69,3 +69,19 @@ EasyNegative, paintings, sketches, (worst quality:2), (low quality:2), (normal q
 rsync -r root@43.130.154.63:/data/code/github/stable-diffusion-webui/outputs/ /Users/sunzhanchao/Downloads/aiputputs/
 
 exit 0
+
+
+photography of a beautiful Chinese girl, 1girl,
+64k, RAW photo, (ultra high res), ultra detailed, ((physically-based rendering)), best quality, solo focus,masterpiece,best quality,official art,
+(realistic, photo-realistic:1.4), (high detailed skin:1.6),(pose:1.4),(detailed face:1.5),
+extremely detailed CG unity 8k wallpaper,
+(long shot:1.3),(fujifilm xt3:1.4),
+<lora:koreanDollLikeness_v10:0.3>,
+<lora:south_v1:0.4>,
+<lora:juan_v1:0.3>,
+(red space suit:1.3),(red armor:1.3), ((beautiful detailed eyes)),(gray hair),
+(soft light:1.3),(global illumination),(studio light),(vivd colors:1.4),vibrant details,
+(WE scene:1.2),(industrial building),
+Negative prompt: (EasyNegative:1.2), ng_deepnegative_v1_75t, paintings, sketches, (worst quality:2), (low quality:2), (normal quality:2), lowres, ((monochrome)), (grayscale:1.2), (skin spots:1.6), acnes, (skin blemishes), (age spot), glans,extra fingers,fewer fingers,(watermark:1.2),(letters:1.2),(nsfw:1.2),teeth,(sexy:1.2),(belly:1.5),(English Text),same face,extra leges,((NASA logo)),(big breast:1.7),((USA flag)),
+(twisted fingers: 1.3), (excessively bent fingers: 1.3),(worst quality, low quality:1.4),((on stomach:2.0)),((tranny)),(interlocked fingers: 1.3),
+Size: 1024x1024, Seed: 1827603352, Model: chilloutmix_NiPrunedFp32Fix, Steps: 108, Sampler: DPM++ 2M Karras, CFG scale: 7, Mask blur: 4, Model hash: fc2511737a, ControlNet Model: control_sd15_openpose [fef5e48e], ControlNet Module: openpose, ControlNet Weight: 1, ControlNet Enabled: True, Denoising strength: 0.75, ControlNet Guidance Strength: 1
--- sync.sh
+++ sync.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 SSHREMOTE=root@43.130.154.63
-while True
-do
-#scp -r ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/out.tar.gz /e/ai/
-#scp -r ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/extensions/sd-webui-additional-networks.tar.gz /e/ai/
-rsync -rvth ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/outputs/ /Users/sunzhanchao/Downloads/aiputputs/
-#rsync -rvth ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/outputs/ /e/ai/outputs/
-echo "slee 15 seconds ..."
-sleep 15
-done
+#while True
+#do
+  #scp -r ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/out.tar.gz /e/ai/
+  #scp -r ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/extensions/sd-webui-additional-networks.tar.gz /e/ai/
+  rsync -rvth ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/outputs/ /Users/sunzhanchao/Downloads/aioutputs/
+  #rsync -rvth ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/outputs/ /e/ai/outputs/
+#echo "slee 15 seconds ..."
+#sleep 15
+#done
 exit 0
