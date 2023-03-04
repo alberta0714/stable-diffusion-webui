7bfaae8c71b0676c3da582b844b1b38355cc0ddd
DESKTOP-OLVEMOF\Administrator <sunzhanchao@126.com>
2023-03-04 08:52:56 +0000

    ac

--- remote_clean.sh
+++ remote_clean.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 SSHREMOTE=root@43.130.154.63
-while True
-do
 #scp -r ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/out.tar.gz /e/ai/
 #scp -r ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/extensions/sd-webui-additional-networks.tar.gz /e/ai/
 #rsync -rvth ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/outputs/ /Users/sunzhanchao/Downloads/aioutputs/
-rsync -rvth ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/outputs/ /cygdrive/e/ai/aioutputs
-echo "slee 3 seconds ..."
-sleep 1
-done
+#rsync -rvth ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/outputs/ /cygdrive/e/ai/aioutputs
+
+ssh ${SSHREMOTE} 'rm -rf /data/code/github/stable-diffusion-webui/outputs/* ; ls /data/code/github/stable-diffusion-webui/outputs/'
+
 exit 0
