4f2fc870302987cf031854731ffaa7ed0363b922
DESKTOP-OLVEMOF\Administrator <sunzhanchao@126.com>
2023-03-06 14:47:25 +0000

    ac

--- shells/sync.sh
+++ shells/sync.sh
@@ -4,8 +4,8 @@ while True
 do
 #scp -r ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/out.tar.gz /e/ai/
 #scp -r ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/extensions/sd-webui-additional-networks.tar.gz /e/ai/
-rsync -rvth ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/outputs/ /Users/sunzhanchao/Downloads/aioutputs/
-#rsync -rvth ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/outputs/ /cygdrive/e/ai/aioutputs
+#rsync -rvth ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/outputs/ /Users/sunzhanchao/Downloads/aioutputs/
+rsync -rvth ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/outputs/ /cygdrive/e/ai/aioutputs
 echo "slee 1 seconds ..."
 sleep 1
 done
