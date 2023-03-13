cf9a385084a671e9a0ebe7ff4e7907eabd6e34b8
DESKTOP-OLVEMOF\Administrator <sunzhanchao@126.com>
2023-03-13 18:07:49 +0100

    ac

--- shells/syncOnce.sh
+++ shells/syncOnce.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 SSHREMOTE=root@43.130.154.63
-rsync -rvth ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/outputs/ /Users/sunzhanchao/Downloads/aioutputs/
-#rsync -rvth ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/outputs/ /cygdrive/e/ai/aioutputs
+#rsync -rvth ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/outputs/ /Users/sunzhanchao/Downloads/aioutputs/
+rsync -rvth ${SSHREMOTE}:/data/code/github/stable-diffusion-webui/outputs/ /cygdrive/e/ai/aioutputs
 exit 0
 
 
